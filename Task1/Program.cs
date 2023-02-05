b88d6ad616af0a714ef83b85fb324045fe168526
Maryna <maryna.lysyak@gmail.com>
2023-02-05 16:36:05 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+ System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 999 || number < 100)
+{
+    Console.Write("Число не трехзначное");
+}
+else
+{
+    System.Console.WriteLine($"Вторая цифра числа  {number/10-(number/100 * 10)}");
+}
+
